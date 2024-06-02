641a6ee0ba91b10119513578e820285cae08be1b
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2024-06-02 12:06:14 +0600

    linkedin lookup agent for url, request data to proxycurl, linkedin data processing by LLM

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,5 +1,36 @@
-import os
+from dotenv import load_dotenv
+from langchain.prompts.prompt import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
 
-if __name__ == '__main__':
-    print("Hello Langchain!")
-    print(os.environ['OPENAI_API_KEY'])
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
+
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+
+    summary_template = """
+    given the Linkedin information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    res = chain.invoke(input={"information": linkedin_data})
+
+    print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Md Mainul Islam")
